58cc240036df24f917a88b91bb045fcfc6bd3fc5
Maxim Molyanov <mm_89615@hotmail.com>
2024-03-12 17:21:41 +0300

    Update window settings, add background

--- pong.py
+++ pong.py
@@ -10,20 +10,30 @@ class Game:
         """Инициализация игры и создание игровых ресурсов"""
         # настройки игрового окна и FPS
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Pong')
+        self.icon = pygame.image.load('images/icon.png')
+        pygame.display.set_icon(self.icon)
         self.clock = pygame.time.Clock()
-
         # основной цикл игры
         self.running = True
 
     def game_running(self):
         """Запуск основного цикла игры"""
         while self.running:
-            self.check_events()
             self.update_screen()
+            self.draw_on_screen()
+            self.check_events()
 
     def update_screen(self):
         """Обновление изображений на экране"""
+        pygame.display.flip()
+        self.draw_on_screen()
         self.clock.tick(FPS)
+
+    def draw_on_screen(self):
+        # прорисовка игрового поля
+        self.screen.fill(BG_COLOR)
+        pygame.draw.line(self.screen, BG_LINE_COLOR, (SCREEN_WIDTH / 2, 0), (SCREEN_WIDTH / 2, SCREEN_HEIGHT))
 
     def check_events(self):
         """Обработка происходящих событий"""
--- settings.py
+++ settings.py
@@ -1,3 +1,7 @@
+# Настройки игрового окна
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 900
 FPS = 60
+# Цвета заднего фона
+BG_COLOR = (33, 166, 69)
+BG_LINE_COLOR = (255, 255, 255)
