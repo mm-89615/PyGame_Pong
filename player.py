dc2ad029a2a07e9fa0003d566a4dabc16ef31bed
Maxim Molyanov <mm_89615@hotmail.com>
2024-03-12 18:07:40 +0300

    add PLAYER.py, moving and drawing

---
+++ player.py
@@ -0,0 +1,51 @@
+import pygame
+from pygame.sprite import Sprite
+
+from settings import *
+
+
+class Player(Sprite):
+    """Класс создания игрока"""
+
+    def __init__(self, player, game_screen):
+        """Инициализация игрока"""
+        super().__init__()
+        self.screen = game_screen.screen
+        self.player = player
+        # Загрузка изображений игроков и размещение на своих местах
+        if self.player == 1:
+            self.image = pygame.image.load('images/player1.png')
+            self.rect = self.image.get_rect()
+            self.center_platform()
+        if self.player == 2:
+            self.image = pygame.image.load('images/player2.png')
+            self.rect = self.image.get_rect()
+            self.center_platform()
+
+    def update(self):
+        """Обновление позиций игроков"""
+        self.check_player_events()
+
+    def check_player_events(self):
+        keys = pygame.key.get_pressed()
+        if self.player == 1:
+            if keys[pygame.K_RIGHT] and self.rect.top >= 10:
+                self.rect.y -= SPEEED_PLAYER
+            if keys[pygame.K_LEFT] and self.rect.bottom <= SCREEN_HEIGHT - 10:
+                self.rect.y += SPEEED_PLAYER
+        if self.player == 2:
+            if keys[pygame.K_d] and self.rect.top >= 10:
+                self.rect.y -= SPEEED_PLAYER
+            if keys[pygame.K_a] and self.rect.bottom <= SCREEN_HEIGHT - 10:
+                self.rect.y += SPEEED_PLAYER
+
+    def blit(self):
+        """Отрисовка корабля в текущей  позиции"""
+        self.screen.blit(self.image, self.rect)
+
+    def center_platform(self):
+        """Размещение платформ в начальной позиции"""
+        if self.player == 1:
+            self.rect.midleft = (0, SCREEN_HEIGHT / 2)
+        if self.player == 2:
+            self.rect.midright = (SCREEN_WIDTH, SCREEN_HEIGHT / 2)
--- pong.py
+++ pong.py
@@ -1,6 +1,7 @@
 import pygame
 
 from settings import *
+from player import Player
 
 
 class Game:
@@ -16,12 +17,20 @@ class Game:
         self.clock = pygame.time.Clock()
         # основной цикл игры
         self.running = True
+        # создание игрока
+        self.player1 = Player(player=1, game_screen=self)
+        self.player2 = Player(player=2, game_screen=self)
 
     def game_running(self):
         """Запуск основного цикла игры"""
         while self.running:
+            # обновление игроков
+            self.player1.update()
+            self.player2.update()
+            # обновление экрана
             self.update_screen()
             self.draw_on_screen()
+            # обработка событий на экране
             self.check_events()
 
     def update_screen(self):
@@ -34,6 +43,9 @@ class Game:
         # прорисовка игрового поля
         self.screen.fill(BG_COLOR)
         pygame.draw.line(self.screen, BG_LINE_COLOR, (SCREEN_WIDTH / 2, 0), (SCREEN_WIDTH / 2, SCREEN_HEIGHT))
+        # прорисовка игроков
+        self.player1.blit()
+        self.player2.blit()
 
     def check_events(self):
         """Обработка происходящих событий"""
