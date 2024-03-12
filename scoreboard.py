a0ecfa63fee51d8c4f478e2c8b7bfeb89199dfbb
Maxim Molyanov <mm_89615@hotmail.com>
2024-03-12 21:56:28 +0300

    update score after goals

---
+++ scoreboard.py
@@ -0,0 +1,25 @@
+import pygame
+
+from settings import *
+
+
+class Scoreboard:
+    """ Класс создания счета игроков """
+
+    def __init__(self, game_screen):
+        """ Инициализация таблицы результатов """
+        self.screen = game_screen.screen
+        # Добавление шрифта
+        self.font = pygame.font.Font('fonts/KodeMono.ttf', FONS_SIZE_SCORE)
+        # Подсчет голов
+        self.player1_goals = 0
+        self.player2_goals = 0
+
+    def draw_scoreboard(self):
+        """ Отрисовка счета игроков """
+        # Отрисовка счета первого игрока
+        self.player1_score = self.font.render(f"{self.player1_goals}", True, SCORE_TEXT_COLOR)
+        self.screen.blit(self.player1_score, ((SCREEN_WIDTH / 4), 20))
+        # Отрисовка счета второго игрока
+        self.player2_score = self.font.render(f"{self.player2_goals}", True, SCORE_TEXT_COLOR)
+        self.screen.blit(self.player2_score, ((SCREEN_WIDTH - SCREEN_WIDTH / 4), 20))
--- settings.py
+++ settings.py
@@ -2,9 +2,12 @@
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 900
 FPS = 60
+# Размер шрифта
+FONS_SIZE_SCORE = 50
 # Цвета заднего фона
 BG_COLOR = (33, 166, 69)
 BG_LINE_COLOR = (255, 255, 255)
+SCORE_TEXT_COLOR = (255, 255, 255)
 # Настройки игрока
 SPEEED_PLAYER = 15
 # Настройки мяча
